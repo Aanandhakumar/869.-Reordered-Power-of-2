5d466664cee18726bcbf7d8b08b04e53b045ac0d
AanandhaKumar M <141841710+Aanandhakumar@users.noreply.github.com>
2025-08-11 00:16:59 +0530

    Another soln

--- code.java
+++ code.java
@@ -1,6 +1,7 @@
 //869. Reordered Power of 2 in java ...
 
-
+// By checking Array is equal after sorting
+// https://leetcode.com/problems/reordered-power-of-2/
 class Solution {
     public boolean reorderedPowerOf2(int n) {
         char[] target = String.valueOf(n).toCharArray();
@@ -18,3 +19,45 @@ class Solution {
         return false;
     }
 }
+
+// Time Complexity: O(31 * log n) ~ O(log n)
+// Space Complexity: O(log n) for storing the characters of the number
+
+
+// Alternative Solution: Using HashMap to count digits
+
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        int[] targetCount = Digitcount(n);
+
+        for(int i = 0; i<31; i++){
+            
+            int[] powerOfTwoCount = Digitcount(1<<i);
+
+            if(matches(targetCount,powerOfTwoCount)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private int[] Digitcount(int n){
+        int[] count = new int[10];
+        while(n>0){
+            count[n%10]++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    private boolean matches(int[] a, int[] b){
+        for(int i = 0; i<10; i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Time Complexity: O(31 * d) ~ O(d) where d is the number of digits in n
+// Space Complexity: O(d) for storing the digit counts
